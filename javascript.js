c36313d193702c22db92f2bbc055294f0a3279f7
Jules KJ <juljen2730@gmail.com>
2024-01-02 17:19:22 +0100

    tilføjet billeder og lavet forside knapper bedre med mouse over og poligoner

--- javascript.js
+++ javascript.js
@@ -8,8 +8,10 @@ document.addEventListener("scroll", function () {
   }
 });
 
+//Her laver jeg et event for nå jeg klikker på min meny
 document.querySelector(" span").addEventListener("click", clickMenuKnap);
 
+//jeg putter animation klassen på menuen
 function clickMenuKnap() {
   console.log("der clickes");
 
@@ -20,7 +22,7 @@ function clickMenuKnap() {
     .removeEventListener("mousedown", clickMenuKnap);
   document.querySelector("span").addEventListener("animationend", clickIgen);
 }
-
+//nå animationen er færdig tager jeg klassen af så jeg kan klikke på den igen og den kan roterer
 function clickIgen() {
   console.log("der kan klikes igen");
   document.querySelector("span").classList = "";
@@ -42,3 +44,25 @@ document.querySelector("#gallery").addEventListener("click", function () {
 document.querySelector("#star_omos").addEventListener("mousedown", function () {
   window.location.href = "om_os.html";
 });
+
+//
+//
+//
+//
+document.querySelector(".star3").addEventListener("mouseover", mouseOver);
+document.querySelector(".star2").addEventListener("mouseover", mouseOver);
+document.querySelector(".star1").addEventListener("mouseover", mouseOver);
+
+function mouseOver() {
+  console.log("musen er over ");
+  this.classList.add("hover");
+
+  this.removeEventListener("mouseocer", mouseOver);
+  this.addEventListener("mouseout", mouseOverIgen);
+}
+
+function mouseOverIgen() {
+  console.log("mouseout");
+
+  this.classList.remove("hover");
+}
